62ee125edbc34d9d1b7025c432a19cbf3b8bab6f
your name <quangdungdao20@gmail.com>
2023-02-20 17:19:41 +0700

    main-techome

---
+++ access/main.js
@@ -0,0 +1,37 @@
+    
+    //  slide Show for container center main
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+    function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    }
+    //  function auto next slider
+    setInterval(function() {
+    slideIndex++;
+    showSlides(slideIndex);
+    }, 3000);
+//
